483150435bcffbe221d20c122e131793c51bc45f
fnajer <lexactr@gmail.com>
2018-07-30 21:21:59 +0300

    Added article template

--- src/components/Welcome/index.jsx
+++ src/components/Welcome/index.jsx
@@ -1,8 +1,12 @@
 import React from 'react';
 
+import Article from './../Article';
+
 class Welcome extends React.Component {
 	render() {
-		return <header className="header header-inverse" style={{backgroundImage: 'url(assets/img/bg-gift.jpg)'}} data-overlay={8}>
+		return (
+			<div>
+				<header className="header header-inverse" style={{backgroundImage: 'url(assets/img/bg-gift.jpg)'}} data-overlay={8}>
 				  <div className="container text-center">
 				    <div className="row">
 				      <div className="col-12 col-lg-8 offset-lg-2">
@@ -12,6 +16,23 @@ class Welcome extends React.Component {
 				    </div>
 				  </div>
 				</header>
+
+				<main class="main-content bg-gray">
+				  <div class="row">
+				    <div class="col-12 col-lg-6 offset-lg-3">
+					  <Article />
+					  <hr />
+					  <Article />
+					  <hr />
+					  <Article />
+					  <hr />
+					  <Article />
+				    </div>
+				  </div>
+				</main>
+
+			</div>
+		);
 	}
 }
 
