5d123039653a4713a6f4a35b34639842d9b7be42
fnajer <lexactr@gmail.com>
2018-07-30 23:00:49 +0300

    Added page "CreateArticle"

--- src/index.js
+++ src/index.js
@@ -5,9 +5,10 @@ import { BrowserRouter, Route, Link } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import Welcome from './components/Welcome';
 import Footer from './components/Footer';
-
+import CreateArticle from './components/CreateArticle';
+ 
 import registerServiceWorker from './registerServiceWorker';
-
+ 
 class Home extends React.Component {
 	render() {
 		return <h1>This is home page</h1>
@@ -27,6 +28,7 @@ ReactDOM.render(
 			<Route exact path="/" component={Welcome} />
 			<Route path="/home" component={Home} />
 			<Route path="/about" component={About} />
+			<Route path="/articles/create" component={CreateArticle} />
 			<Footer />
 		</div>
 	</BrowserRouter>, 
